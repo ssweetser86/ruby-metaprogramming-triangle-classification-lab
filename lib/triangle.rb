0e289a4433225c631038bed9a73cba1a54457ff8
Stephen <stephenjsweets@gmail.com>
2020-11-19 15:03:18 -0500

    commit tree fbdd246bc754ea3ca04b7bc77416610fedf29ff3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_reader :sides
+
+  def initialize(a, b, c)
+    @sides = [a, b, c].sort
+  end
+
+  def kind
+    if @sides[0] <= 0 || @sides[0]+@sides[1] <= @sides[2]
+      raise TriangleError
+    end
+    if @sides[0] == @sides[2]
+      return :equilateral
+    elsif @sides[1] == @sides[2] && @sides[0] < @sides[1]
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "illegal triangle given"
+    end
+  end
 end
